3b53f3cb3fbd68f630ce3d06ac94bf8790317ff1
Oliver Ovalles <ovallesoliver01@gmail.com>
2025-05-27 06:24:58 -0400

    Filtering learned

--- nba.py
+++ nba.py
@@ -5,26 +5,18 @@ df = pd.read_csv(r'NBA\PlayerIndex_nba_stats.csv')
 
 nueva_tabla = df.loc[:,['PLAYER_FIRST_NAME','TEAM_NAME','HEIGHT','COUNTRY']]
 
-counter_of_less_than_6 = 0
-
 
 for n in range(len(nueva_tabla['HEIGHT'])):
     nueva_tabla.at[n, 'NEW_HEIGHT'] = float(str(nueva_tabla.at[n,'HEIGHT']).replace('-','.'))
     
     
-more_than_7_df = pd.DataFrame()
 
-for i in range(len(nueva_tabla['NEW_HEIGHT'])):
-    if nueva_tabla.at[i,'NEW_HEIGHT'] > 7.0:
-        more_than_7_df = nueva_tabla.loc[:,['PLAYER_FIRST_NAME','NEW_HEIGHT','COUNTRY']]
+more_than_7 = nueva_tabla["NEW_HEIGHT"] > 7
         
-        
-print(more_than_7_df)
-        
-   
+print(nueva_tabla[more_than_7])
         
 
 
 
 
-
+ 
